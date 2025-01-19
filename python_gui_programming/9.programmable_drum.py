634be53c4564be870473ff93ef6c7d47acd3aa72
alireza-hashemii <alirezahashemi.dev@gmail.com>
2025-01-19 14:22:48 +0330

    skeleton of drum machine

---
+++ python_gui_programming/9.programmable_drum.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+
+class DrumMachine():
+    def app(self):
+        self.window = tk.Tk()
+
+        self.window.mainloop()
+
+
+
+
+
+
+if __name__ == '__main__':
+    drum_sample = DrumMachine()
+    drum_sample.app()
