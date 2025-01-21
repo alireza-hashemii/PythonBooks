eacb4f0e78502f34080fc42cd3ce0daeaac19b19
alireza-hashemii <alirezahashemi.dev@gmail.com>
2025-01-21 12:40:17 +0330

    works fine except for the part of playing sound. since pymedia has some  problems

--- python_gui_programming/9.programmable_drum.py
+++ python_gui_programming/9.programmable_drum.py
@@ -1,8 +1,15 @@
 from tkinter import *
-
+from tkinter import dialog
+import os
+import winsound
 
 class DrumMachine():
     MAX_DRUM_NUM = 5
+    def __init__(self):
+       
+        self.widget_drum_name = []
+        self.widget_drum_file_name = [0] * self.MAX_DRUM_NUM
+        self.current_drum_no = 0
 
     def app(self):
         self.window = Tk()
@@ -12,6 +19,7 @@ class DrumMachine():
         self.create_left_bar()
         self.crate_right_bar()
         self.create_play_bar()
+        self.play_audio('file1.ogg')
         self.window.mainloop()
 
 
@@ -42,17 +50,20 @@ class DrumMachine():
 
 
     def create_left_bar(self):
+        
         left_frame = Frame(self.window)
         left_frame.grid(row=10, column=0,
         columnspan=6, sticky=W+E+N+S)
 
         tbicon = PhotoImage(file='images/oopenfile.gif')
         for i in range(10, 15):
-            button = Button(left_frame, image=tbicon)
+            button = Button(left_frame, image=tbicon,command=lambda: self.drum_load(i-10))
             button.image = tbicon
+            button.command = self.drum_load(i - 10)
             button.grid(row=i, column=0, padx=5, pady=2)
             self.drum_entry = Entry(left_frame)
             self.drum_entry.grid(row=i, column=4, padx=7, pady=2)
+            self.widget_drum_name.append(self.drum_entry)
     
 
     def crate_right_bar(self):
@@ -76,7 +87,7 @@ class DrumMachine():
     def button_clicked(self,i,j,bpu):
         def callback():
             btn = self.button[i][j]
-            color = 'grey55' if (j/bpu)%2 else 'khaki'
+            color = 'grey55' if (j/bpu) % 2 else 'khaki'
             new_color = 'green' if btn.cget('bg') != 'green' else color
             btn.config(bg=new_color)
             return callback
@@ -97,7 +108,29 @@ class DrumMachine():
         loopbutton.grid(row=ln, column=16, padx=1)
 
 
+    def drum_load(self, drum_no):
+        def callback():
+            self.current_drum_no = drum_no
+            try:
+                file_name = dialog.askopenfilename(
+                defaultextension=".wav", filetypes=[("Wave Files","*.wav"),("OGG Files","*.ogg")])
+                if not file_name: 
+                    return None
+                try:
+                    del self.widget_drum_file_name[drum_no]
+                except: 
+                    pass
+                self.widget_drum_file_name.insert(drum_no, file_name)
+                drum_name = os.path.basename(file_name)
+                self.widget_drum_name[drum_no].delete(0, END)
+                self.widget_drum_name[drum_no].insert(0, drum_name)
+            except:
+                dialog.showerror('Invalid', "Error loading drum samples")
+            return callback
+        
+
 
 if __name__ == '__main__':
+    winsound.PlaySound('file1.ogg',winsound.SND_FILENAME)
     drum_sample = DrumMachine()
     drum_sample.app()        
